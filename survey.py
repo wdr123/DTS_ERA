44c912f1a0133e62464c0e12798da480a86d1555
Dingrong_Wang <dw7445@rit.edu>
2022-07-21 12:53:52 -0400

    ignore

---
+++ survey.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+import pandas as pd
+
+dir = './data/dataset0'
+
+
+def get_length_from_path(self, a):
+    l1_start = 0
+    l1_end = len(pd.read_csv(a).to_numpy())
+    return [l1_start, l1_end]
+
+
+def
+os.listdir(dir)
