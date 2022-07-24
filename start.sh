8ff1d46dde63f87b026e8c510c98b3142c8d0398
Dingrong_Wang <dw7445@rit.edu>
2022-07-23 20:39:12 -0400

    seed20-partial

--- start.sh
+++ start.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
+
 for i in {0..20..1}
 do
-  python RAM_ASD_TD_mean_train_test.py --identifier mean --seed $i
+#  if [ $1 == "mean" ]
+#  then
+#    python RAM_ASD_TD_mean_train_test.py --identifier mean --seed $i
+#  elif [ $1 == "partial"]
+#  then
+#    python RAM_ASD_TD_partial_train_test.py --identifier partial --seed $i
+#  else
+#    python RAM_ASD_TD_partial_train_test.py --seed $i
+#  fi
+
+  if [ $1 = "mean" ]
+  then
+    python RAM_ASD_TD_mean_train_test.py --identifier mean --seed $i
+  elif [ $1 = "partial" ]
+  then
+    python RAM_ASD_TD_partial_train_test.py --identifier partial --seed $i
+  else
+    python RAM_ASD_TD_partial_train_test.py --seed $i
+  fi
+
 done
